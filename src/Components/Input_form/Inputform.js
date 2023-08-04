f9fa7d1e9fca3d58ec6d5858e0a56eb5a407dd70
Devamchaudhari <devamchaudhari40@gmail.com>
2023-08-04 15:59:54 +0530

    fix: checkmark todo data issue | remove: empty file | formating change

--- src/Components/Input_form/Inputform.js
+++ src/Components/Input_form/Inputform.js
@@ -1,11 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import TodoList from '../Todolists/Todolists';
 
-
 function InputField(props) {
-
   const [todoList, setTodoList] = useState(() => {
-    const savedTodos = localStorage.getItem("todoList");
+    const savedTodos = localStorage.getItem('todoList');
     if (savedTodos) {
       return JSON.parse(savedTodos);
     } else {
@@ -13,15 +11,15 @@ function InputField(props) {
     }
   });
 
-  const [todo, setTodo] = useState("");
+  const [todo, setTodo] = useState('');
   const [editIndex, setEditIndex] = useState(-1);
 
-  // Remove Data functionality 
-  const removeData = (index) => {
+  // Remove Data functionality
+  const removeData = (index, checkArrIndex) => {
     const newTodos = [...todoList];
     newTodos.splice(index, 1);
     setTodoList(newTodos);
-
+    localStorage.removeItem('checkedTask', JSON.stringify(checkArrIndex));
   };
 
   // Edit data functionality
@@ -29,53 +27,47 @@ function InputField(props) {
     const newTodos = todoList[index];
     setTodo(newTodos);
     setEditIndex(index);
-  }
-
-  // console.log('index edit :>> ', todo);
+  };
 
   const submitChanges = (e) => {
     e.preventDefault();
 
     if (editIndex > 0 || editIndex >= 0) {
       todoList[editIndex] = todo;
-      setTodoList([...todoList])
-      setTodo("");
-
+      setTodoList([...todoList]);
+      setTodo('');
     } else {
       setTodoList([...todoList, todo]);
-      setTodo("");
+      setTodo('');
     }
     setEditIndex(-1);
-  }
-
+  };
 
   useEffect(() => {
-    localStorage.setItem("todoList", JSON.stringify(todoList));
-  }, [todoList]
-  );
-
+    localStorage.setItem('todoList', JSON.stringify(todoList));
+  }, [todoList]);
 
   return (
-    <div className='Inp-Container'>
-      <form className='task-Container' onSubmit={submitChanges}>
-
+    <div className="Inp-Container">
+      <form className="task-Container" onSubmit={submitChanges}>
         <input
-          className='inp-field'
+          className="inp-field"
           type={props.type}
           placeholder={props.placeholder}
           onChange={(e) => setTodo(e.target.value)}
-          value={todo} required />
-        <button className='Add-btn'>{editIndex > -1 ? "Update Task" : "Add task"}</button>
+          value={todo}
+          required
+        />
+        <button className="Add-btn">
+          {editIndex > -1 ? 'Update Task' : 'Add task'}
+        </button>
       </form>
 
-      {
-        todoList.length > 0 ?
-          (<TodoList
-            list={todoList}
-            removeData={removeData}
-            editData={editData} />) :
-          (<p className='added-task'>No task to show</p>)
-      }
+      {todoList.length > 0 ? (
+        <TodoList list={todoList} removeData={removeData} editData={editData} />
+      ) : (
+        <p className="added-task">No task to show</p>
+      )}
     </div>
   );
 }
--- src/Components/Todolists/Todolists.js
+++ src/Components/Todolists/Todolists.js
@@ -1,64 +1,76 @@
-import React, { useState,useEffect} from 'react';
-import { TrashCanIcon, EditIcon, UncheckedIcon, CheckIcon } from "../Icons/Icons";
+import React, { useState, useEffect } from 'react';
+import {
+  TrashCanIcon,
+  EditIcon,
+  UncheckedIcon,
+  CheckIcon,
+} from '../Icons/Icons';
+
 function TodoList(props) {
-     const [checkedArr,setCheckedArr]= useState(()=>{
-        const savedCheckTodos = localStorage.getItem("checkedTask");
-                if (savedCheckTodos) {
-                return JSON.parse(savedCheckTodos);
-                } else {
-                return [];
-                }
-          });
+  const [checkedArr, setCheckedArr] = useState(() => {
+    const savedCheckTodos = localStorage.getItem('checkedTask');
+    if (savedCheckTodos) {
+      return JSON.parse(savedCheckTodos);
+    } else {
+      return [];
+    }
+  });
 
-     
-     
-     
-     const handleCheck =(index)=>{
-        if (checkedArr.indexOf(index)>-1) {
-            let newArr= [...checkedArr];
-            let finalIndex = newArr.findIndex((ele)=>ele===index);
-            newArr.splice(finalIndex,1);
-            setCheckedArr(newArr);
-        } else {
-            setCheckedArr([...checkedArr,index]);
-            localStorage.removeItem("checkedTask",JSON.stringify(checkedArr));
-        }
-    }   
-  
+  const handleCheck = (index) => {
+    if (checkedArr.indexOf(index) > -1) {
+      let newArr = [...checkedArr];
+      let finalIndex = newArr.findIndex((ele) => ele === index);
+      newArr.splice(finalIndex, 1);
+      setCheckedArr(newArr);
+    } else {
+      setCheckedArr([...checkedArr, index]);
+      localStorage.removeItem('checkedTask', JSON.stringify(checkedArr));
+    }
+  };
 
-    useEffect(() => {
-    localStorage.setItem("checkedTask", JSON.stringify(checkedArr));
-  }, [checkedArr]
+  useEffect(() => {
+    localStorage.setItem('checkedTask', JSON.stringify(checkedArr));
+  }, [checkedArr]);
+  return (
+    <div className="task-container">
+      <ul className="list-container">
+        {props.list.map((addTodo, index) => (
+          <li key={index} className="added-task">
+            <button
+              className="uncheck-btn"
+              onClick={() => handleCheck(`${index}`)}
+              style={{
+                textDecoration:
+                  checkedArr.indexOf(`${index}`) > -1 ? `line-through` : `none`,
+              }}
+            >
+              {checkedArr.indexOf(`${index}`) > -1 ? (
+                <CheckIcon className="uncheck-icon" />
+              ) : (
+                <UncheckedIcon className="uncheck-icon" />
+              )}
+
+              {addTodo}
+            </button>
+            <div className="edit-del-wraper">
+              <button
+                className="delete-btn"
+                onClick={() => props.removeData(index, checkedArr)}
+              >
+                <TrashCanIcon className="delete-icon" />
+              </button>
+              <button
+                className="Edit-btn"
+                onClick={() => props.editData(index)}
+              >
+                <EditIcon className="Edit-icon" />
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
-     return (
-        <div className="task-container">
-            <ul className='list-container'>
-                {
-                    props.list.map((addTodo, index) => (
-                        <li key={index} className="added-task">
-                            <button className='uncheck-btn' 
-                                onClick={() => handleCheck(`${index}`)} 
-                                style={{
-                                    textDecoration: checkedArr.indexOf(`${index}`)>-1 ? `line-through` : `none`
-                                }}
-                            >
-                                { checkedArr.indexOf(`${index}`)>-1 ?
-                                    <CheckIcon className="uncheck-icon" /> :
-                                    <UncheckedIcon className="uncheck-icon" /> 
-                                }
-                                
-                                {addTodo}
-                            </button>
-                            <div className="edit-del-wraper">
-                                <button className='delete-btn' onClick={() => props.removeData(index)}><TrashCanIcon className="delete-icon" /></button>
-                                <button className="Edit-btn" onClick={() => props.editData(index)}><EditIcon className="Edit-icon" /></button>
-                            </div>
-                        </li>
-                    ))
-                }
-            </ul>
-        </div>
-    );
 }
 
 export default TodoList;
